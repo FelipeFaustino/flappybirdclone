24e6a568b286edcc0a916f6ffc6c6e8c468e3a8d
FelipeFaustino <71053689+FelipeFaustino@users.noreply.github.com>
2020-11-13 20:27:19 -0300

    Update flappy.js

--- flappy.js
+++ flappy.js
@@ -1,7 +1,22 @@
 console.log('Flappy Bird');
 
 const sprites = new Image();
-sprites.src = './sprites.png';
+sprites.src = 'sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+function loop(){
+  contexto.drawImage(
+    sprites,
+    0, 0, //posição em x e y
+    33, 24, //recorte do sprite
+    10, 50, 
+    33, 24
+  );
+
+
+  requestAnimationFrame();
+}
+
+loop();
