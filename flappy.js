bb23b4d65a908ab4f2ebc9854139fb12839e1764
FelipeFaustino <71053689+FelipeFaustino@users.noreply.github.com>
2021-01-18 19:42:57 -0300

    teste plano de fundo não desenha

--- flappy.js
+++ flappy.js
@@ -1,10 +1,10 @@
-console.log('Flappy Bird');
+console.log("Flappy Bird");
 
 const sprites = new Image();
-sprites.src = 'sprites.png';
+sprites.src = "sprites.png";
 
-const canvas = document.querySelector('canvas');
-const contexto = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const contexto = canvas.getContext("2d");
 
 //código para cenário
 const planoDeFundo = {
@@ -13,24 +13,18 @@ const planoDeFundo = {
   largura: 275,
   altura: 204,
   x: 0,
-  y: canvas.height = 204,
-  desenho(){
-    contexto.fillStyle = '#70c5ce';
-    contexto.fillRect(0,0, canvas.width, canvas.height);
-    
-    contexto.drawImage(
-     sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
-      planoDeFundo.largura, planoDeFundo.altura,
-      planoDeFundo.x, planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
+  y: canvas.height - 204,
+  desenho() {
     contexto.drawImage(
       sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
-      planoDeFundo.largura, planoDeFundo.altura,
-      (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
+      planoDeFundo.spriteX,
+      planoDeFundo.spriteY,
+      planoDeFundo.largura,
+      planoDeFundo.altura,
+      planoDeFundo.x,
+      planoDeFundo.y,
+      planoDeFundo.largura,
+      planoDeFundo.altura
     );
   }
 };
@@ -40,47 +34,60 @@ const chao = {
   spriteX: 0,
   spriteY: 610,
   largura: 224,
-  altura:112,
+  altura: 112,
   x: 0,
   y: canvas.height - 112,
   desenho() {
     contexto.drawImage(
       sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      chao.x, chao.y,
-      chao.largura, chao.altura,
+      chao.spriteX,
+      chao.spriteY,
+      chao.largura,
+      chao.altura,
+      chao.x,
+      chao.y,
+      chao.largura,
+      chao.altura
     );
-    
-    contexto.drawImage( //redesenha o chão, porém completa a lacuna do lado direito
+
+    contexto.drawImage(
+      //redesenha o chão, porém completa a lacuna do lado direito
       sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      (chao.x + chao.largura), chao.y,
-      chao.largura, chao.altura,
+      chao.spriteX,
+      chao.spriteY,
+      chao.largura,
+      chao.altura,
+      chao.x + chao.largura,
+      chao.y,
+      chao.largura,
+      chao.altura
     );
-  },
+  }
 };
 
-const flappyBird ={
+const flappyBird = {
   spriteX: 0,
   spriteY: 0,
   largura: 33,
   altura: 24,
   x: 10,
   y: 50,
-  desenho(){
+  desenho() {
     contexto.drawImage(
       sprites,
-      flappyBird.spriteX, flappyBird.spriteY, //posição em x e y
-      flappyBird.largura, flappyBird.altura, //recorte do sprite
-      flappyBird.x, flappyBird.y, 
-      flappyBird.largura, flappyBird.altura,
+      flappyBird.spriteX,
+      flappyBird.spriteY, //posição em x e y
+      flappyBird.largura,
+      flappyBird.altura, //recorte do sprite
+      flappyBird.x,
+      flappyBird.y,
+      flappyBird.largura,
+      flappyBird.altura
     );
   }
-}
+};
 
-function loop(){
+function loop() {
   flappyBird.desenho();
   chao.desenho();
   requestAnimationFrame(loop);
